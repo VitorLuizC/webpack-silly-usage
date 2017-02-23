169d2892b42d2562a1e6e9bfe6d45b7e20cd11eb
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2017-02-23 18:16:52 -0300

    Add scripts structure + webpack
    Added scripts structure that consists into src, src/lib a bundle (src/index) and a dist with bundled/transpiled files.

---
+++ src/script/main.js
@@ -0,0 +1,14 @@
+const carousel = require('./lib/carousel');
+
+
+
+carousel()
+
+document.addEventListener('', initialize);
+
+/**
+ * 
+ */
+function initialize() {
+	const carousel = document.querySelector('#carousel');
+}
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const { optimize } = require('webpack');
+
+const config =  {
+  entry: './src/script/main.js',
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, './dist/js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.styl$/,
+        use: ['css-loader', 'style-loader', 'stylus-loader']
+      },
+      {
+        test: /\.js$/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  target: "web"
+};
+
+module.exports = function (env) {
+  if (env === 'production') {
+    config.plugins = [new optimize.UglifyJsPlugin()];
+    config.devtool = false;
+  }
+
+  return config;
+};
